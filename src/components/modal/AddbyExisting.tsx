7393365895c593dae7e216381c6b8ebeee4d1fa9
Takuhei Mizutani <takuheim1011@gmail.com>
2021-10-07 21:27:15 +0900

    something

--- src/components/modal/AddbyExisting.tsx
+++ src/components/modal/AddbyExisting.tsx
@@ -1,21 +1,21 @@
-import React, { useState } from "react";
-import { useEffect } from "react";
-import styled, { keyframes } from "styled-components";
-import client, { Spotify } from "../../queries/api/spotify/api";
+import React, { useState } from 'react'
+import { useEffect } from 'react'
+import styled, { keyframes } from 'styled-components'
+import client, { Spotify } from '../../queries/api/spotify/api'
 import {
   useGetAllPlaylists,
   useGetAllSpotifyPlaylist,
   useGetUser,
-} from "../../queries/hooks";
-import { Collection } from "../../types/types";
-import { List, Item as ItemStyles, Loading } from "../Shared";
-import { AddPlaylistForm } from "../AddPlaylistForm";
+} from '../../queries/hooks'
+import { Collection } from '../../types/types'
+import { List, Item as ItemStyles, Loading } from '../Shared'
+import { AddPlaylistForm } from '../AddPlaylistForm'
 
 const Item = styled(ItemStyles)`
   &:hover {
     cursor: pointer;
   }
-`;
+`
 
 const slideIn = keyframes`
   from {
@@ -27,51 +27,51 @@ const slideIn = keyframes`
     height: auto;
     opacity: 1;
   } 
-`;
+`
 
 const Wrapper = styled.div`
   animation: ${slideIn} 0.5s both;
   height: 100%;
-`;
+`
 
 export const AddbyExisting: React.FC<{
-  handleFetch: (collection: Collection) => void;
-  noSpotify?: boolean;
+  handleFetch: (collection: Collection) => void
+  noSpotify?: boolean
 }> = ({ handleFetch, noSpotify }) => {
-  const { data: spotifyPlaylists, isLoading } = useGetAllSpotifyPlaylist();
-  const { data: miiixtapPlaylists } = useGetAllPlaylists();
-  const { data: userInfo } = useGetUser();
-  const [playlists, setPlaylists] = useState<Collection[]>([]);
+  const { data: spotifyPlaylists, isLoading } = useGetAllSpotifyPlaylist()
+  const { data: miiixtapPlaylists } = useGetAllPlaylists()
+  const { data: userInfo } = useGetUser()
+  const [playlists, setPlaylists] = useState<Collection[]>([])
 
   useEffect(() => {
-    if (!spotifyPlaylists || !miiixtapPlaylists) return;
+    if (!spotifyPlaylists || !miiixtapPlaylists) return
     if (noSpotify) {
-      setPlaylists(miiixtapPlaylists);
-      return;
+      setPlaylists(miiixtapPlaylists)
+      return
     }
-    setPlaylists(spotifyPlaylists);
-  }, [noSpotify, spotifyPlaylists, miiixtapPlaylists]);
+    setPlaylists(spotifyPlaylists)
+  }, [noSpotify, spotifyPlaylists, miiixtapPlaylists])
 
   const handleClick = async (playlistInfo: Collection) => {
-    if (!userInfo) return;
-    let playlist = playlistInfo;
+    if (!userInfo) return
+    let playlist = playlistInfo
     if (!noSpotify) {
       playlist = await Spotify.getPlaylist(
         playlistInfo?.playlistInfo.id,
         client(userInfo?.access_token)
-      );
+      )
     }
-    handleFetch(playlist);
-  };
+    handleFetch(playlist)
+  }
 
-  if (isLoading) return <Loading />;
+  if (isLoading) return <Loading />
 
   return (
     <Wrapper>
       {playlists && playlists.length > 0 ? (
         <List>
-          <AddPlaylistForm className={"create-new"}>
-            <span style={{ marginRight: "8px" }}>Create new </span>
+          <AddPlaylistForm className={'create-new'}>
+            <span style={{ marginRight: '8px' }}>Create new </span>
           </AddPlaylistForm>
 
           {playlists.map((playlist) => {
@@ -82,14 +82,14 @@ export const AddbyExisting: React.FC<{
               >
                 <p>{playlist.playlistInfo.name}</p>
               </Item>
-            );
+            )
           })}
         </List>
       ) : (
-        <p style={{ padding: "2px 24px" }}>
+        <p style={{ padding: '2px 24px' }}>
           You don&apos;t have any spotify playlists to import
         </p>
       )}
     </Wrapper>
-  );
-};
+  )
+}
